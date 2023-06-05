77db2efa00ed41d3a104761317f300eeec510503
EnzoOestanto <enzo.oestanto@gmail.com>
2023-06-05 10:29:39 +0700

    edit prod controller

--- backend/seeders/20230605025613-product.js
+++ backend/seeders/20230605025613-product.js
@@ -2,7 +2,7 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     /**
      * Add seed commands here.
      *
@@ -12,14 +12,72 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
     */
+    await queryInterface.bulkInsert('products', [
+      {
+        name: 'Nasi Sapi Original',
+        category: 'food',
+        imageURL: '',
+        price: 29000,
+        status: true,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Nasi Sapi Yainiku',
+        category: 'food',
+        imageURL: '',
+        price: 29000,
+        status: true,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Nasi Ayam Original',
+        category: 'food',
+        imageURL: '',
+        price: 23000,
+        status: true,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Nasi Ayam Pedas',
+        category: 'food',
+        imageURL: '',
+        price: 25000,
+        status: true,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Sate Ayam',
+        category: 'sideDish',
+        imageURL: '',
+        price: 7000,
+        status: true,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Sate Kulit',
+        category: 'sideDish',
+        imageURL: '',
+        price: 7000,
+        status: true,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+
+    ], {});
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
      *
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
+    await queryInterface.bulkDelete('products', null, {})
   }
 };
