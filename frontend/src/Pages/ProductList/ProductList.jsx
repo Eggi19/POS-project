10635d0c6462c87201d0eb1b8bf20c49f4bec11d
EnzoOestanto <enzo.oestanto@gmail.com>
2023-06-05 23:18:42 +0700

    PP-2

--- frontend/src/Pages/ProductList/ProductList.jsx
+++ frontend/src/Pages/ProductList/ProductList.jsx
@@ -40,11 +40,11 @@ export default function ProductList() {
                     )
                 })}
             </div>
-            <div className="relative">
-                <div className=" absolute justify-center bottom-3">
-                    <PaginationControlled handlePagination={setPagination} />
-                </div>
+
+            <div className="pt-5">
+                <PaginationControlled handlePagination={setPagination} />
             </div>
+
         </div>
 
     )
