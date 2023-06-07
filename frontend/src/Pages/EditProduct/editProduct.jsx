9476e873e1e3ff1495997617e218221c2a7f2fd5
Eggi19 <eggiyapari19@gmail.com>
2023-06-07 17:13:56 +0800

    update PP-6

--- frontend/src/Pages/EditProduct/editProduct.jsx
+++ frontend/src/Pages/EditProduct/editProduct.jsx
@@ -11,11 +11,30 @@ import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
 import Fab from '@mui/material/Fab';
 import EditIcon from '@mui/icons-material/Edit';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import EditComponent from './EditComponent/editComponent';
 import { useState } from 'react';
 import { deleteProduct, getAllProductsWithCategory } from '../../API/productAPI';
 import { useEffect } from 'react';
 
 export default function EditProduct() {
+    //Dialog
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+
     const [dataProducts, setDataProducts] = useState([])
 
     const getDataProducts = async () => {
@@ -27,12 +46,12 @@ export default function EditProduct() {
         }
     }
 
-    const onDeleteProduct = async(productId)=> {
+    const onDeleteProduct = async (productId) => {
         try {
             await deleteProduct(productId)
             getDataProducts()
         } catch (error) {
-            
+
         }
     }
 
@@ -40,51 +59,66 @@ export default function EditProduct() {
         getDataProducts()
     }, [])
     return (
-        <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Image</TableCell>
-                        <TableCell align="left">Name</TableCell>
-                        <TableCell align="left">Category</TableCell>
-                        <TableCell align="left">Price</TableCell>
-                        <TableCell align="left">Stock</TableCell>
-                        <TableCell align="left"></TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {dataProducts.map((value) => (
-                        <TableRow
-                            key={value.id}
-                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                        >
-                            {console.log(value)}
-                            <TableCell component="th" scope="row">
-                                <img className='w-24' src={value.imageURL} alt="" />
-                            </TableCell>
-                            <TableCell align="left">{value.name}</TableCell>
-                            <TableCell align="left">{value.Category?.name}</TableCell>
-                            <TableCell align="left">{value.price}</TableCell>
-                            {
-                                value.status ?
-                                    <TableCell align="left">Available</TableCell>
-                                    :
-                                    <TableCell align="left">Unavailable</TableCell>
-                            }
-                            <TableCell>
-                                <Stack direction="row" alignItems="center" spacing={1}>
-                                    <Fab size="small" color="secondary" aria-label="edit">
-                                        <EditIcon />
-                                    </Fab>
-                                    <IconButton aria-label="delete" size="small" onClick={() => onDeleteProduct(value.id)} >
-                                        <DeleteIcon />
-                                    </IconButton>
-                                </Stack>
-                            </TableCell>
+        <>
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Image</TableCell>
+                            <TableCell align="left">Name</TableCell>
+                            <TableCell align="left">Category</TableCell>
+                            <TableCell align="left">Price</TableCell>
+                            <TableCell align="left">Stock</TableCell>
+                            <TableCell align="left"></TableCell>
                         </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+                    </TableHead>
+                    <TableBody>
+                        {dataProducts.map((value) => (
+                            <TableRow
+                                key={value.id}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            >
+                                {console.log(value)}
+                                <TableCell component="th" scope="row">
+                                    <img className='w-24' src={value.imageURL} alt="" />
+                                </TableCell>
+                                <TableCell align="left">{value.name}</TableCell>
+                                <TableCell align="left">{value.Category?.name}</TableCell>
+                                <TableCell align="left">{value.price}</TableCell>
+                                {
+                                    value.status ?
+                                        <TableCell align="left">Available</TableCell>
+                                        :
+                                        <TableCell align="left">Unavailable</TableCell>
+                                }
+                                <TableCell>
+                                    <Stack direction="row" alignItems="center" spacing={1}>
+                                        <Fab size="small" color="secondary" aria-label="edit" onClick={handleClickOpen} >
+                                            <EditIcon />
+                                        </Fab>
+                                        <IconButton aria-label="delete" size="small" onClick={() => onDeleteProduct(value.id)} >
+                                            <DeleteIcon />
+                                        </IconButton>
+                                    </Stack>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>Subscribe</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        To subscribe to this website, please enter your email address here. We
+                        will send updates occasionally.
+                    </DialogContentText>
+                    <EditComponent />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>Close</Button>
+                </DialogActions>
+            </Dialog>
+        </>
     );
 }
