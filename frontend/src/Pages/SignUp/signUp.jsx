6d3107ce9869cb8e5d6da2d0547742622d8f1f5f
Eggi19 <eggiyapari19@gmail.com>
2023-06-08 17:01:13 +0800

    update PP-14

--- frontend/src/Pages/SignUp/signUp.jsx
+++ frontend/src/Pages/SignUp/signUp.jsx
@@ -107,6 +107,7 @@ export default function SignUp() {
                             </Grid>
                             <Grid item xs={12}>
                                 <TextField
+                                    required
                                     fullWidth
                                     id="Role"
                                     select
