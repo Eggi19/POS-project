93859aaeb8b3cd250188df1bd3cc67310d583455
EnzoOestanto <enzo.oestanto@gmail.com>
2023-06-11 10:22:04 +0700

    PP-10

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,3 +1,35 @@
-const chekcRole = async(req,res,next)=>{
+const jwt = require('jsonwebtoken')
 
+module.exports = {
+    verifyUser: (req, res, next) => {
+        console.log('masuk verify user');
+        let token = req.headers
+        console.log('token', token)
+        if (!token) {
+            return res.status(401).send('accsess denied1')
+        }
+        try {
+            token = token.split(' ')[1]
+            if (token === null || !token) {
+                return res.status(401).send('accsess denied2')
+            }
+            let verifiedUser = jwt.verify(token, 'adminVerification')
+            if (!verifiedUser) {
+                return res.status(401).send('accsess denied3')
+            } else {
+                req.user = verifiedUser
+                next()
+            }
+        } catch (error) {
+            res.send('invalid token')
+        }
+    },
+
+ checkRole: async (req, res, next) => {
+        if (req.user?.role === 'admin') {
+            next()
+        } else {
+            return res.status(401).send('accsess denied')
+        }
+    }
 }
