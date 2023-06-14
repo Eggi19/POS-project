2c1aa7469231af7f076ccd3dd6f74202063b4006
EnzoOestanto <enzo.oestanto@gmail.com>
2023-06-14 10:26:59 +0700

    PP-23

--- backend/routers/productRouter.js
+++ backend/routers/productRouter.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const Router = express.Router()
 const { productController } = require('../controllers')
-// const { verifyUser } = require('../middleware/auth')
-
 
 Router.get('/', productController.getProduct)
 Router.post('/', productController.createProduct)
--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -1,8 +1,9 @@
-const {userController} = require('../controllers')
+const { userController } = require('../controllers')
+const { verifyUser, checkRole } = require('../middleware/auth')
 const express = require('express')
 const Router = express.Router()
 
-Router.post('/', userController.createUser)
+Router.post('/', verifyUser, checkRole, userController.createUser)
 Router.get('/', userController.getUserData)
 
 module.exports = Router
